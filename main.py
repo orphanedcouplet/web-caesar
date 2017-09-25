9615f381fa604f774c668014196708b96f94dd6f
orphanedcouplet <oliverdayers@gmail.com>
2017-09-25 14:03:35 -0500

    created form

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- create your form here -->
+        <form action-"" method="post">
+            <label for="rot">
+                Rotate by:
+                <input type="text" name="rot" />
+            </label>
+            <textarea name="text" rows="10" cols="30"></textarea>
+            <input type="submit" value="Submit Query" />
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
